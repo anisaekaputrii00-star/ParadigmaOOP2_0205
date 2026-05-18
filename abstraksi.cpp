bf5045015986e67befb4ff600b52e8f494120866
anisaekaputrii00-star <anisaekaputrii00@gmail.com>
2026-05-18 17:47:08 +0700

    tambahkan method display

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -13,4 +13,11 @@ class AbstraksiKlas
         x = a;
         y = b;
     }
-}
+    //menampilkan nilai
+    void display()
+    {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+};
+
